02f6128744ede3c3fa7ff06ba072d10b173becbb
Christopher Siems and Illia Kuchko <csiems@gmail.com>
2016-03-17 16:12:01 -0700

    add ItemActivity with product image

--- app/src/main/java/com/epicodus/mysteryapp/ItemActivity.java
+++ app/src/main/java/com/epicodus/mysteryapp/ItemActivity.java
@@ -2,12 +2,30 @@ package com.epicodus.mysteryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ItemActivity extends AppCompatActivity {
+    @Bind(R.id.productImage) ImageView productImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+        ButterKnife.bind(this);
+
+        int position = getIntent().getIntExtra("product", -1);
+        if (position != -1) {
+            Picasso.with(ItemActivity.this)
+                    .load(StoreActivity.finalPictureArray[position])
+                    .noFade()
+                    .resize(800,800)
+                    .centerCrop()
+                    .into(productImage);
+        }
     }
 }
--- app/src/main/java/com/epicodus/mysteryapp/StoreActivity.java
+++ app/src/main/java/com/epicodus/mysteryapp/StoreActivity.java
@@ -1,11 +1,15 @@
 package com.epicodus.mysteryapp;
 
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,6 +18,7 @@ import butterknife.ButterKnife;
 
 public class StoreActivity extends AppCompatActivity {
     ArrayList<Integer> pictureResIdArray = new ArrayList<>();
+    static Integer[] finalPictureArray;
     @Bind(R.id.gridView) GridView mGridView;
     private TypedArray mProducts;
 
@@ -23,17 +28,27 @@ public class StoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_store);
         ButterKnife.bind(this);
 
-        mGridView.setAdapter(new PictureAdapter(StoreActivity.this, prepareArrayOfIds()));
+        prepareArrayOfIds();
+
+        mGridView.setAdapter(new PictureAdapter(StoreActivity.this, finalPictureArray));
+
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Intent intent = new Intent(StoreActivity.this, ItemActivity.class);
+                intent.putExtra("product", position);
+                startActivity(intent);
+            }
+        });
     }
 
-    public Integer[] prepareArrayOfIds() {
+    public void prepareArrayOfIds() {
         mProducts = getResources().obtainTypedArray(R.array.products);
         for (int i = 0; i < mProducts.length(); i++) {
             pictureResIdArray.add(mProducts.getResourceId(i, -1));
         }
 
-        Integer finalPictureArray[] = new Integer[pictureResIdArray.size()];
+        finalPictureArray = new Integer[pictureResIdArray.size()];
         finalPictureArray = pictureResIdArray.toArray(finalPictureArray);
-        return finalPictureArray;
     }
 }
